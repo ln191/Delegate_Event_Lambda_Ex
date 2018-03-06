c0ce568c1a7ce899c75a3e79d7e89bfeee7e917c
Lucas A.Nielsen <lucas>
2018-03-06 16:24:13 +0100

    updated readme file, and completed assignment

--- Delegate_shipping_ex/Program.cs
+++ Delegate_shipping_ex/Program.cs
@@ -6,10 +6,96 @@ using System.Threading.Tasks;
 
 namespace Delegate_shipping_ex
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public delegate double shippingTax(int itemPrice);
+
+        private static void Main(string[] args)
         {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Enter the zone you wish to ship to:");
+                string zone = Console.ReadLine();
+                if (zone.Equals("exit"))
+                {
+                    exit = true;
+                    return;
+                }
+                Console.WriteLine("Enter the value of the shipment:");
+                string valueStr = Console.ReadLine();
+                int value = int.Parse(valueStr);
+
+                shippingTax taxZoneCal = ZoneSelection(zone);
+
+                double fee = taxZoneCal(value);
+
+                Console.WriteLine($"The shipping fee for {zone} is {fee}$");
+                Console.WriteLine();
+            }
         }
+
+        /// <summary>
+        /// Gets the right tax calculation function to the given zone
+        /// </summary>
+        /// <param name="zone"></param>
+        /// <returns>Delegate tax function</returns>
+        private static shippingTax ZoneSelection(string zone)
+        {
+            shippingTax selectedZone = null;
+            switch (zone)
+            {
+                case "zone1":
+                    selectedZone = ZoneOne;
+                    break;
+
+                case "zone2":
+                    selectedZone = ZoneTwo;
+                    break;
+
+                case "zone3":
+                    selectedZone = ZoneThree;
+                    break;
+
+                case "zone4":
+                    selectedZone = ZoneFour;
+                    break;
+            }
+            return selectedZone;
+        }
+
+        /// <summary>
+        /// Zone 1 25% fee
+        /// </summary>
+        /// <param name="itemPrice"></param>
+        /// <returns></returns>
+        private static double ZoneOne(int itemPrice) => PercentageOf(itemPrice, 25.00);
+
+        /// <summary>
+        /// Zone 2 12% + 25$ fee
+        /// </summary>
+        /// <param name="itemPrice"></param>
+        /// <returns></returns>
+        private static double ZoneTwo(int itemPrice) => PercentageOf(itemPrice, 12.00) + 25;
+
+        /// <summary>
+        /// Zone 3 8%
+        /// </summary>
+        /// <param name="itemPrice"></param>
+        /// <returns></returns>
+        private static double ZoneThree(int itemPrice) => PercentageOf(itemPrice, 8.00);
+
+        /// <summary>
+        /// Zone 4 4% + 25$
+        /// </summary>
+        /// <param name="itemPrice"></param>
+        /// <returns></returns>
+        private static double ZoneFour(int itemPrice) => PercentageOf(itemPrice, 4.00) + 25;
+
+        /// <summary>
+        /// Gets the given percentage amount of the given value
+        /// </summary>
+        /// <returns>Percentage of given value</returns>
+        private static double PercentageOf(int value, double percentage) => (value / 100) * percentage;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Delegate_shipping_ex
 Demonstrates the use of delegates in C#
+
+##Assignment
+Create shipping fee calculator: 
+
+	-	Ship the items to different destinations.
+	-	Shipping fees are x% of the given item price.
+	-	Some places have an additional risk fee.
+	
+	Task: 
+	Given a destination and item price, calculate the shipping fee.
+	
+##Zone Details
+
+	Zone 1		25%
+	Zone 2		12% is high risk
+	Zone 3 		 8%
+	Zone 4		 4% is high risk
+	
+	High risk fee 25$ on top of percentage fee. 
