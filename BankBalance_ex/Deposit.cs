55935bef5dbfc5f32bb2a674ecf474feb8d0e6d6
Lucas A.Nielsen <lucas>
2018-03-09 13:33:22 +0100

    added bankBalance project for event ex

---
+++ BankBalance_ex/Deposit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBalance_ex
+{
+    public class Deposit
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+    }
+}
